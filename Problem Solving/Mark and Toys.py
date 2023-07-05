a3c3f5b4aca124e2cc02aa34aa961bcf4121d99b
Dravichi Code <64124548+Dra-Co@users.noreply.github.com>
2023-07-05 17:01:09 +0530

    Create Mark and Toys.py

---
+++ Problem Solving/Mark and Toys.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    i = 0
+    prices.sort()
+    while k - prices[i] >= 0:
+        k -= prices[i]
+        i += 1
+    return i
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
