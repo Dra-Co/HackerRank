254c3bc946a038d70fbfd88b884d832a86253607
Dravichi Code <64124548+Dra-Co@users.noreply.github.com>
2023-07-08 10:09:39 +0530

    Create Weighted Uniform Strings.py

---
+++ Problem Solving/Weighted Uniform Strings.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedUniformStrings' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER_ARRAY queries
+#
+
+def weightedUniformStrings(s, queries):
+    # Write your code here
+    res = set()
+    prev = ''
+    for i in s:
+        if i != prev:
+            prev = i
+            count = 0
+        count += 1
+        res.add(count * (ord(i) - 96))
+    return ["Yes" if i in res else "No" for i in queries]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = weightedUniformStrings(s, queries)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
