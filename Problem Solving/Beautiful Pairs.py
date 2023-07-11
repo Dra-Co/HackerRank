2e6e947c0d1d5bd465b4e70fb0201d67b6198ee6
Dravichi Code <64124548+Dra-Co@users.noreply.github.com>
2023-07-11 22:40:00 +0530

    Create Beautiful Pairs.py

---
+++ Problem Solving/Beautiful Pairs.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY A
+#  2. INTEGER_ARRAY B
+#
+
+def beautifulPairs(A, B):
+    # Write your code here
+    res = 0
+    for i in set(A):
+        res += min(A.count(i), B.count(i))
+    return res-1 if res == len(A) else res + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    A = list(map(int, input().rstrip().split()))
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = beautifulPairs(A, B)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
