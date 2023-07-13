b61ad398e6ebf927dd85ded2ce2ad993bbdbada4
Dravichi Code <64124548+Dra-Co@users.noreply.github.com>
2023-07-13 10:21:00 +0530

    Create String Validators.py

---
+++ Python/String Validators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    s = input()
+    res = [False] * 5
+    for i in s:
+        if i.isalnum(): res[0] = True
+        if i.isalpha(): res[1] = True
+        if i.isdigit(): res[2] = True
+        if i.islower(): res[3] = True
+        if i.isupper(): res[4] = True
+    print(*res, sep="\n")
